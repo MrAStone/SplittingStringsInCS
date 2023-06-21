8741dfd588f4b4580b6dd981695f00761241e8e7
A Stone <astone@heckgrammar.co.uk>
2023-06-21 09:32:18 +0100

    Add project files.

---
+++ SplittingStringsInCS/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SplittingStringsInCS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "This,String,Is,Split,Up,Through,The,Use,Of,Commas";
+            string[] inputSplit = input.Split(',');
+            foreach (string line in inputSplit)
+            {
+                Console.WriteLine(line);
+            }
+
+        }
+    }
+}
